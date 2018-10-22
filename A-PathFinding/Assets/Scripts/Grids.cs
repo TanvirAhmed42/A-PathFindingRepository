5830b8a3b0f4676acc0bf64f8d2b16fac0b9ff79
TanvirAhmed42 <43964962+TanvirAhmed42@users.noreply.github.com>
2018-10-22 19:45:43 +0600

    A* Implemented
    So now given a start and target position the gizmos will show the shortest path between them using A*.

--- A-PathFinding/Assets/Scripts/Grids.cs
+++ A-PathFinding/Assets/Scripts/Grids.cs
@@ -30,9 +30,30 @@ public class Grids : MonoBehaviour {
 			for (int y = 0; y < gridSizeY; y++) {
 				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
 				bool walkable = !Physics.CheckSphere (worldPoint, nodeRadius, unWalkableMask);
-				grid [x, y] = new Node (walkable, worldPoint);
+				grid [x, y] = new Node (walkable, worldPoint, x, y);
 			}
 		}
+	}
+
+	public List<Node> GetNeighbours (Node node) {
+		List<Node> neighbours = new List<Node> ();
+
+		for (int x = -1; x <= 1; x++) {
+			for (int y = -1; y <=1; y++) {
+				if (x == 0 && y == 0) {
+					continue;
+				}
+
+				int checkX = node.gridX + x;
+				int checkY = node.gridY + y;
+
+				if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY) {
+					neighbours.Add (grid [checkX, checkY]);
+				}
+			}
+		}
+
+		return neighbours;
 	}
 
 	public Node NodeFromWorldPoint (Vector3 worldPosition) {
@@ -47,12 +68,18 @@ public class Grids : MonoBehaviour {
 		return grid [x, y];
 	}
 
+	public List<Node> path;
 	void OnDrawGizmos () {
 		Gizmos.DrawWireCube (transform.position, new Vector3 (gridWorldSize.x, 1f, gridWorldSize.y));
 
 		if (grid != null) {
 			foreach (Node node in grid) {
 				Gizmos.color = node.walkable ? Color.white : Color.red;
+				if (path != null) {
+					if (path.Contains (node)) {
+						Gizmos.color = Color.black;
+					}
+				}
 				Gizmos.DrawCube (node.worldPosition, Vector3.one * (nodeDiameter - .1f));
 			}
 		}
